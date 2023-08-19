e6c9c1d1e08bbd1553259224b82f7d4ca7f8992a
Robert Hawkins <hawkrobe@gmail.com>
2023-08-19 09:40:36 -0400

    derivatives p015

---
+++ transfer.sh
@@ -0,0 +1,131 @@
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-p015* ./bids/derivatives/
+datalad save -m 'derivatives p015'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting p016'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-p016* ./bids/derivatives/
+datalad save -m 'derivatives p016'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting p017'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-p017* ./bids/derivatives/
+datalad save -m 'derivatives p017'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting p018'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-p018* ./bids/derivatives/
+datalad save -m 'derivatives p018'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting p019'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-p019* ./bids/derivatives/
+datalad save -m 'derivatives p019'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting p020'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-p020* ./bids/derivatives/
+datalad save -m 'derivatives p020'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting p021'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-p021* ./bids/derivatives/
+datalad save -m 'derivatives p021'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting p022'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-p022* ./bids/derivatives/
+datalad save -m 'derivatives p022'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting s022'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s022* ./bids/derivatives/
+datalad save -m 'derivatives s022'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting s021'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s021* ./bids/derivatives/
+datalad save -m 'derivatives s021'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting s020'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s020* ./bids/derivatives/
+datalad save -m 'derivatives s020'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting s019'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s019* ./bids/derivatives/
+datalad save -m 'derivatives s019'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting s018'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s018* ./bids/derivatives/
+datalad save -m 'derivatives s018'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting s017'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s017* ./bids/derivatives/
+datalad save -m 'derivatives s017'
+datalad push --to dropbox-fmri
+datalad push --to origin
+echo 'starting s016'
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s016* ./bids/derivatives/
+datalad save -m 'derivatives s016'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s015* ./bids/derivatives/
+datalad save -m 'derivatives s015'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s014* ./bids/derivatives/
+datalad save -m 'derivatives s014'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s013* ./bids/derivatives/
+datalad save -m 'derivatives s013'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s012* ./bids/derivatives/
+datalad save -m 'derivatives s012'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s011* ./bids/derivatives/
+datalad save -m 'derivatives s011'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s010* ./bids/derivatives/
+datalad save -m 'derivatives s010'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s009* ./bids/derivatives/
+datalad save -m 'derivatives s009'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s008* ./bids/derivatives/
+datalad save -m 'derivatives s008'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s007* ./bids/derivatives/
+datalad save -m 'derivatives s007'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s006* ./bids/derivatives/
+datalad save -m 'derivatives s006'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s005* ./bids/derivatives/
+datalad save -m 'derivatives s005'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s004* ./bids/derivatives/
+datalad save -m 'derivatives s004'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s003* ./bids/derivatives/
+datalad save -m 'derivatives s003'
+datalad push --to dropbox-fmri
+datalad push --to origin
+cp -r ../neuro-tangrams/data/bids/derivatives/sub-s002* ./bids/derivatives/
+datalad save -m 'derivatives s002'
+datalad push --to dropbox-fmri
+datalad push --to origin
+
